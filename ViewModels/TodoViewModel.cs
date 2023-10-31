7bf3214bbddaa3959a5d39e20165de9c5c669bc3
Lucas6331 <lukasspfc8@gmail.com>
2023-10-30 22:22:59 -0300

    Added todo class

---
+++ ViewModels/TodoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVM_API_SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_API_SampleProject.ViewModels
+{
+    public class TodoViewModel
+    {
+        public Todo Todo { get; set; }
+
+        public TodoViewModel()
+        {
+            Todo = new Todo()
+            {
+                userId = 1,
+                id = 1,
+                title = "delectus aut autem",
+                completed = false,
+            };
+        }
+    }
+}
---
+++ Views/TodoView.xaml.cs
@@ -0,0 +1,15 @@
+using MVVM_API_SampleProject.Models;
+using MVVM_API_SampleProject.ViewModels;
+
+namespace MVVM_API_SampleProject.Views;
+
+public partial class TodoView : ContentPage
+{
+	public TodoView()
+	{
+		InitializeComponent();
+		BindingContext = new TodoViewModel();
+	}
+
+    
+}
